d245613b4df03363e9f8b41dfc498f2c83550cfe
Robert Topolowski <robert.topolowski@kcl.ac.uk>
2022-10-02 20:49:57 +0100

    Implement median determination functionality

--- median.py
+++ median.py
@@ -1,12 +1,25 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+if len(numbers) % 2 == 0:
+    # Even number of elements in "numbers"
+    key = int(len(numbers) / 2)
+    val1 = numbers[key]
+    val2 = numbers[key - 1]
+    print((val1 + val2) / 2)
+else:
+    # Odd number of elements in "numbers"
+    key = int(math.floor(len(numbers) / 2))
+    print(numbers[key])
